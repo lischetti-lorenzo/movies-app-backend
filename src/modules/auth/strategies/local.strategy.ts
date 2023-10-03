54d715f1b4464df1d173d47243016f308d820d50
Lorenzo Lischetti <lischetti.lorenzo@gmail.com>
2023-10-03 19:13:01 -0300

    Added message when user or password is incorrect

--- src/modules/auth/strategies/local.strategy.ts
+++ src/modules/auth/strategies/local.strategy.ts
@@ -19,7 +19,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     const user = await this.authService.validateUser(username, password);
 
     if (user === null) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException('Username or password incorrect');
     }
 
     return user;
